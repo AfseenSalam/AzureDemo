68fb2848bbe6916d29a02982ed6f6e06aee73061
Afseen Salam <afseenfazil@gmail.com>
2024-10-07 16:10:14 -0400

    google login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,31 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),
+    //We are building a provider. Version 2.2.0 of package doesn't provide one.
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              //Remove the .apps.googleusercontent.com from the client id
+              //MAKE SURE TO HIDE IT FROM GITHUB
+              "884550303174-2ok5aif5o551jt5faarkvj0polg9oqv9"
+            ),
+          },
+        ],
+        onError: (err) => {
+          debugger;
+          console.error(err);
+        },
+      } as SocialAuthServiceConfig,
+    },
+    //more providers can go here
+  ]
 };
